9cb48478ffaf2258597b530067a10d8526bcd57c
dandanmania <danielkimus@gmail.com>
2022-09-15 15:24:28 -0700

    Add rudimentary navbar

--- components/navbar.js
+++ components/navbar.js
@@ -1,7 +1,9 @@
 export default function Navbar() {
     return(
-        <div>
-            <p>Hello</p>
+        <div style={{backgroundColor: 'white', color: 'black', height: '4rem', display: 'flex'}}>
+            <p style={{margin: 0, padding: '1rem'}}>Title</p>
+            <span style={{flex: '1 1 auto'}}></span>
+            <p style={{margin: 0, padding: '1rem'}}>Login</p>
         </div>
     )
 }
